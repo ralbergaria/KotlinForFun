969cca649390bbb96141dfc624cb4fe0a50e3c17
ralbergaria <rafael-albergaria.carmo@kis.krieger.de>
2021-06-03 13:55:54 +0200

    Add setup files

---
+++ src/main/kotlin/de/kotlinForFun/rafael/RafaelApplication.kt
@@ -0,0 +1,11 @@
+package de.kotlinForFun.rafael
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RafaelApplication
+
+fun main(args: Array<String>) {
+	runApplication<RafaelApplication>(*args)
+}
